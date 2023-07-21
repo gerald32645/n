90740ddcf3817682027546aa6b56c3a2c90a55d8
gerald32645 <140191633+gerald32645@users.noreply.github.com>
2023-07-21 18:57:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 import asyncio
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -15,27 +16,38 @@ channel_id = 1131985734761459803
 # The specific message you want to send and delete
 specific_message = "lol"
 
+is_messaging = False
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    await send_and_delete_specific_message()
 
-async def send_and_delete_specific_message():
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!start'):
+        global is_messaging
+        if not is_messaging:
+            is_messaging = True
+            await send_and_delete_specific_message(message.channel)
+
+    await bot.process_commands(message)
+
+async def send_and_delete_specific_message(channel):
     guild = bot.get_guild(guild_id)
     if not guild:
         print(f"Guild with ID {guild_id} not found.")
         return
 
-    channel = guild.get_channel(channel_id)
-    if not channel:
+    target_channel = guild.get_channel(channel_id)
+    if not target_channel:
         print(f"Channel with ID {channel_id} not found.")
         return
 
-    while True:
+    while is_messaging:
         try:
-            message = await channel.send(specific_message)
+            message = await target_channel.send(specific_message)
             await message.delete()
-            await asyncio.sleep(1)  # SECOUNDS  TO WAIT BETWEEN EATCH MESSAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            await asyncio.sleep(1)  # SECONDS TO WAIT BETWEEN EACH MESSAGE
         except discord.HTTPException as e:
             if e.code == 429:
                 retry_after = e.retry_after
@@ -49,5 +61,5 @@ async def send_and_delete_specific_message():
             print(f"Unexpected error: {e}")
 
 # Start the bot
-keep_alive()
 bot.run(os.environ['TOKEN'])
+keep_alive()
